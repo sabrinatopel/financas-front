da06efe9f35ee9543870ce10a61937d4f550f4ad
“Sabrina <sabrina.f.topel@gmail.com>
2025-01-05 15:00:09 -0300

    feat: implementando os btns de cada rota do header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { RoutePaths } from "@/routes/AppRouter";
 
 export default function TemplateDemo() {
   const navigate = useNavigate();
@@ -8,26 +8,33 @@ export default function TemplateDemo() {
       label: "Home",
       icon: "pi pi-home",
       command: () => {
-        navigate('/about')
-    }
+        navigate(RoutePaths.HOME.path);
+      },
     },
     {
       label: "Compras",
       icon: "pi pi-star",
+      command: () => {
+        navigate(RoutePaths.COMPRAS.path);
+      },
     },
     {
       label: "Cartão de Credito",
       icon: "pi pi-star",
+      command: () => {
+        navigate(RoutePaths.CARTAO_CREDITO.path);
+      },
     },
     {
       label: "Banco",
       icon: "pi pi-cog",
+      command: () => {
+        navigate(RoutePaths.BANCO.path);
+      },
     },
   ];
 
-  const start = (
-    <h4>Finanças</h4>
-  );
+  const start = <h4>Finanças</h4>;
 
   return (
     <div className="card">
