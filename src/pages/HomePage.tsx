00944642db81e9a5c511a9e802c7ae47a2c03b57
“Sabrina <sabrina.f.topel@gmail.com>
2025-01-05 15:35:15 -0300

    feat: tirando o header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,44 +1,10 @@
 import React from "react";
-import { RoutePaths } from "@/routes/AppRouter";
 
-export default function TemplateDemo() {
-  const navigate = useNavigate();
-  const items: MenuItem[] = [
-    {
-      label: "Home",
-      icon: "pi pi-home",
-      command: () => {
-        navigate(RoutePaths.HOME.path);
-      },
-    },
-    {
-      label: "Compras",
-      icon: "pi pi-star",
-      command: () => {
-        navigate(RoutePaths.COMPRAS.path);
-      },
-    },
-    {
-      label: "Cartão de Credito",
-      icon: "pi pi-star",
-      command: () => {
-        navigate(RoutePaths.CARTAO_CREDITO.path);
-      },
-    },
-    {
-      label: "Banco",
-      icon: "pi pi-cog",
-      command: () => {
-        navigate(RoutePaths.BANCO.path);
-      },
-    },
-  ];
-
-  const start = <h4>Finanças</h4>;
-
+export default function HomePage() {
   return (
-    <div className="card">
-      <Menubar model={items} start={start} />
+    <div>
+     oi
     </div>
   );
 }
+
